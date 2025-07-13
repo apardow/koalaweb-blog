5dec7d0ab53f890910b9ccb9bf255a5cbfd2ff21
apardow <pardow@gmail.com>
2025-07-13 17:25:49 -0400

    Corrige error en página Acerca

--- app/acerca/page.js
+++ app/acerca/page.js
@@ -2,7 +2,8 @@ export default function PaginaAcerca() {
   return (
     <main>
       <h1>Página de Acerca de</h1>
-      <p>Este es el contenido de la página "Acerca de".</p>
+      {/* Usamos comillas simples para que no haya conflicto con las de adentro */}
+      <p>Este es el contenido de la página 'Acerca de'.</p>
     </main>
   );
 }
