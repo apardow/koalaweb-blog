8128e211cf5b81ff92c1598ac5ed28ca83df9523
apardow <pardow@gmail.com>
2025-07-13 17:37:41 -0400

    se cambiaron las comillas

--- app/acerca/page.js
+++ app/acerca/page.js
@@ -3,7 +3,7 @@ export default function PaginaAcerca() {
     <main>
       <h1>Página de Acerca de</h1>
       {/* Usamos comillas simples para que no haya conflicto con las de adentro */}
-      <p>Este es el contenido de la página 'Acerca de'.</p>
+      <p>Este es el contenido de la página Acerca de.</p>
     </main>
   );
 }
