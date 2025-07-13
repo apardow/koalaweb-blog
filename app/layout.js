1a8f361e566cf53b5ebf8abf679008772be35fc0
apardow <pardow@gmail.com>
2025-07-13 17:33:37 -0400

    Corrige comillas en página Acerca

--- app/layout.js
+++ app/layout.js
@@ -7,19 +7,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang='es'>
       <body>
 
 <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc', marginBottom: '1rem' }}>
-  <Link href="/" style={{ marginRight: '1rem' }}>
+  <Link href='/' style={{ marginRight: '1rem' }}>
     Inicio
   </Link>
-  <Link href="/acerca" style={{ marginRight: '1rem' }}>
+  <Link href='/acerca' style={{ marginRight: '1rem' }}>
     Acerca de
   </Link>
 
   {/* ESTE ES EL ENLACE NUEVO */}
-  <Link href="/blog">
+  <Link href='/blog'>
     Blog
   </Link>
 </nav>
